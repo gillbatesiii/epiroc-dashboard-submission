d590a0dc769ccea2a241db4c02ee9f8c800dfe1a
Brian Tan <bharveytan@gmail.com>
2024-12-20 22:05:19 -0800

    eslint

--- backend/src/routes/motor.js
+++ backend/src/routes/motor.js
@@ -1,13 +1,13 @@
-import express from "express";
+import express from 'express';
 
-import motorController from "../controllers/motor.js";
+import motorController from '../controllers/motor.js';
 
 const router = express.Router();
 
 router.get('/rpm', motorController.getRPM);
-router.put("/rpm", (req, res) => {
-    console.log('req', req);
-    res.send("Motor rpm updated");
+router.put('/rpm', (req, res) => {
+  console.log('req', req);
+  res.send('Motor rpm updated');
 });
 
 export default router;
