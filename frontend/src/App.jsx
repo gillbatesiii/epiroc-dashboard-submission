cebaa77966dc703f1930cbe8716f5ad5e734e358
Brian Tan <bharveytan@gmail.com>
2024-12-20 22:22:52 -0800

    frontend: get rpm from api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Tachometer from "./Tachometer.jsx";
+import client from "./utils.js";
 
 function App() {
   const [count, setCount] = useState(0)
     const [rpm, setRpm] = useState(0);
+
+    useEffect(() => {
+        client.get('/motor/rpm').then((res) => setRpm(res.data.rpm));
+    }, []);
   return (
       <div>
           <input onChange={(e) => setRpm(e.target.value)}/>
